3894776324fa0c405023509b5c5b10955ba2d7bc
kevinph00 <kevinpham@live.se>
2021-09-20 21:11:31 +0200

    Skickar in alla filer från master

---
+++ app/src/main/java/com/example/tabtest3/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.tabtest3.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.tabtest3.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = startFragment()
+            1 -> fragment = kartaFragment()
+            2 -> fragment = statistikFragment()
+        }
+        return fragment!!
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 3 total pages.
+        return 3
+    }
+}
