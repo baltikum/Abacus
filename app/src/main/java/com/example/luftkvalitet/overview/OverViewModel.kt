a00cb31e725be1ac91e36e2f7764357412b8336a
kossamu <larssonjohnny@outlook.com>
2021-09-30 13:59:31 +0200

    Testar en commit igen som jag ej vill ska uppdatera.

--- app/src/main/java/com/example/luftkvalitet/overview/OverViewModel.kt
+++ app/src/main/java/com/example/luftkvalitet/overview/OverViewModel.kt
@@ -1,14 +1,13 @@
 package com.example.luftkvalitet.overview
 
+import android.graphics.Color
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.luftkvalitet.databinding.FragmentStartBinding
 import com.example.luftkvalitet.network.*
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
 
 class OverViewModel : ViewModel() {
@@ -25,26 +24,86 @@ class OverViewModel : ViewModel() {
         viewModelScope.launch {
             val dataList = api.getHourlyData(station, date, time, "")
             // clear all text
-            binding.showInfo1.text = ""
-            binding.showInfo2.text = ""
-            binding.showInfo3.text = ""
-            binding.showInfo4.text = ""
-            binding.showInfo5.text = ""
-            binding.showInfo6.text = ""
-            binding.showInfo7.text = ""
+            binding.showStation.text = ""
+            binding.showLatitude.text = ""
+            binding.showLongitude.text = ""
+            binding.showNo2.text = ""
+            binding.showNox.text = ""
+            binding.showPm25.text = ""
+            binding.showPm10.text = ""
 
             if (dataList.size > 0) {
 
-                binding.showInfo1.text = dataList[0].station
-                binding.showInfo2.text = dataList[0].latitude_wgs84
-                binding.showInfo3.text = dataList[0].longitude_wgs84
+                binding.showStation.text = dataList[0].station
+                binding.showLatitude.text = dataList[0].latitude_wgs84
+                binding.showLongitude.text = dataList[0].longitude_wgs84
+
+
+                /*
+                if(dataList[0].latitude_wgs84.toDouble() > 57.71){
+                   binding.showLatitude.setTextColor(Color.RED)
+                } else {
+                    binding.showLatitude.setTextColor(Color.GREEN)
+                }
+
+                 */
+
 
                 for (data in dataList) {
                     when (data.parameter) {
-                        "NO2" -> binding.showInfo4.text = data.raw_value
-                        "NOx" -> binding.showInfo5.text = data.raw_value
-                        "PM2" -> binding.showInfo6.text = data.raw_value
-                        "PM10" -> binding.showInfo7.text = data.raw_value
+                        "NO2" -> {
+                            if(data.raw_value.toDouble() < (36)){
+                                binding.showNo2.setTextColor(Color.GREEN)
+                            } else if (data.raw_value.toDouble() > 48) {
+                                binding.showNo2.setTextColor(Color.RED)
+                            } else {
+                                binding.showNo2.setTextColor(Color.parseColor ("#D1D100"))
+                            }
+                            /*
+                          var test = 10
+                          if(test < 4){
+                              binding.showNo2.setTextColor(Color.GREEN)
+                          } else if (test > 12) {
+                              binding.showNo2.setTextColor(Color.RED)
+                          } else {
+                              binding.showNo2.setTextColor(Color.parseColor ("#D1D100"))
+                          }
+                          binding.showNo2.text = data.raw_value
+*/
+                        }
+
+                        "NOx" -> {
+                            if(data.raw_value.toDouble() < 4){
+                                binding.showNox.setTextColor(Color.GREEN)
+                            } else if (data.raw_value.toDouble() > 12) {
+                                binding.showNox.setTextColor(Color.RED)
+                            } else {
+                                binding.showNox.setTextColor(Color.parseColor ("#D1D100"))
+                            }
+                            binding.showNox.text = data.raw_value
+                        }
+
+                        "PM2" -> {
+                            if(data.raw_value.toDouble() < 9){
+                                binding.showPm25.setTextColor(Color.GREEN)
+                            } else if (data.raw_value.toDouble() > 17) {
+                                binding.showPm25.setTextColor(Color.RED)
+                            } else {
+                                binding.showPm25.setTextColor(Color.parseColor ("#D1D100"))
+                            }
+                            binding.showPm25.text = data.raw_value
+                        }
+
+                        "PM10" -> {
+                            if(data.raw_value.toDouble() < 25){
+                                binding.showPm10.setTextColor(Color.GREEN)
+                            } else if (data.raw_value.toDouble() > 35) {
+                                binding.showPm10.setTextColor(Color.RED)
+                            } else {
+                                binding.showPm10.setTextColor(Color.parseColor ("#D1D100"))
+                            }
+                            binding.showPm10.text = data.raw_value
+                        }
                     }
                 }
             } else {
--- app/src/main/java/com/example/luftkvalitet/ui/main/startFragment.kt
+++ app/src/main/java/com/example/luftkvalitet/ui/main/startFragment.kt
@@ -1,28 +1,38 @@
 package com.example.luftkvalitet.ui.main
 
+import android.R.attr
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import com.example.luftkvalitet.R
+
 import com.example.luftkvalitet.databinding.FragmentStartBinding
 import com.example.luftkvalitet.overview.OverViewModel
-import android.widget.ArrayAdapter
 
 
-
-import android.widget.Spinner
-
-
+import android.R.attr.country
+import android.R
+import android.widget.*
 
 
 val overViewModel = OverViewModel()
 
     class startFragment : Fragment() {
+
+
+        // values for spinner
+        val values = arrayOf<String>(
+            "Femman",
+            "Haga Norra",
+            "Lejonet",
+            "Mobil 1",
+            "Mobil 2",
+            "Mobil 3"
+        )
+
+
 
         private var _binding: FragmentStartBinding? = null
 
@@ -30,6 +40,7 @@ val overViewModel = OverViewModel()
 
         override fun onCreateView(inflater: LayoutInflater,
                                   container: ViewGroup?,
+
                                   savedInstanceState: Bundle?): View {
 
             _binding = FragmentStartBinding.inflate(inflater, container, false)
@@ -38,53 +49,75 @@ val overViewModel = OverViewModel()
 
             val date = "2021-09-17"
             val time = "22:00*"
-            binding.button.setOnClickListener {
-                overViewModel.updateStationData("Femman", date, time, binding)
-            }
-            binding.button2.setOnClickListener {
-                overViewModel.updateStationData("Haga Norra", date, time, binding)
-            }
-            binding.button3.setOnClickListener {
-                overViewModel.updateStationData("Lejonet", date, time, binding)
-            }
-
-            binding.button4.setOnClickListener {
-                overViewModel.updateStationData("Mobil 1", date, time, binding)
-            }
-
-            binding.button5.setOnClickListener {
-                overViewModel.updateStationData("Mobil 2", date, time, binding)
-            }
-
-            binding.button6.setOnClickListener {
-                overViewModel.updateStationData("Mobil 3", date, time, binding)
-            }
 
 
-            val values = arrayOf(
-                "Femman",
-                "Haga Norra",
-                "Lejonet",
-                "Mobil 1",
-                "Mobil 2",
-                "Mobil 3"
-            )
+
+            /* old spinner test
             val adapter =
                 ArrayAdapter(this.requireActivity(), android.R.layout.simple_spinner_item, values)
             adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
             binding.spinner.adapter = adapter;
+            */
+
+           // val spin = findViewById<Spinner>(R.id.coursesspinner)
+           // binding.spinner.setOnItemSelectedListener {}
+
+            //  spin.onItemSelectedListener = this
+
+            val spin = binding.spinner
+
+            val adapter =
+                ArrayAdapter(this.requireActivity(), android.R.layout.simple_spinner_item, values)
+            adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+            spin.adapter = adapter;
 
 
 
 
 
+            spin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+                    when (position) {
+                        0 -> overViewModel.updateStationData("Femman", date, time, binding)
+                        1 -> overViewModel.updateStationData("Haga Norra", date, time, binding)
+                        2 -> overViewModel.updateStationData("Lejonet", date, time, binding)
+                        3 -> overViewModel.updateStationData("Mobil 1", date, time, binding)
+                        4 -> overViewModel.updateStationData("Mobil 2", date, time, binding)
+                        5 -> overViewModel.updateStationData("Mobil 3", date, time, binding)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
 
 
+
+
+
+            /*
+            when (values[0]) {
+                 "Femman" -> {
+                    overViewModel.updateStationData("Femman", date, time, binding)
+                }
+                "Haga Norra" -> {
+                    overViewModel.updateStationData("Femman", date, time, binding)
+                }
+            }
+*/
 
 
 
             return view
         }
+
+
+
+
+
+
 
 
 
