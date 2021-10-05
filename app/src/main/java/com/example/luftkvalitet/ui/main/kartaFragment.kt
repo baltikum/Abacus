941ec6e1387d19d1de0b04ec2a2e0f27e9b50f9c
Lukvargen <lukas.magnusson@hotmail.com>
2021-10-05 15:07:14 +0200

    google maps

--- app/src/main/java/com/example/luftkvalitet/ui/main/kartaFragment.kt
+++ app/src/main/java/com/example/luftkvalitet/ui/main/kartaFragment.kt
@@ -7,9 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.luftkvalitet.R
 import com.example.luftkvalitet.databinding.FragmentKartaBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class kartaFragment : Fragment(){
+class kartaFragment : Fragment(), OnMapReadyCallback{
 
     private var _binding: FragmentKartaBinding? = null
     private val binding get() = _binding!!
@@ -29,9 +36,23 @@ class kartaFragment : Fragment(){
             permission.getLocation(this.requireActivity(),binding)
         }
 
+        println("activity")
+        println(requireActivity())
+        val mapFragment = requireActivity().supportFragmentManager
+            .findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
         return view
     }
 
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
+    }
 
 
 
