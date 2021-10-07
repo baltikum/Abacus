7ec319eefc3bd090f731280226c1fcb144ca4317
kossamu <larssonjohnny@outlook.com>
2021-10-07 10:14:40 +0200

    bartchart som kan byta värden o x o y axeln

--- app/src/main/java/com/example/luftkvalitet/ui/main/statistikFragment.kt
+++ app/src/main/java/com/example/luftkvalitet/ui/main/statistikFragment.kt
@@ -1,5 +1,6 @@
 package com.example.luftkvalitet.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,21 +10,24 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.annotation.ColorRes
 import com.example.luftkvalitet.R
 import com.example.luftkvalitet.databinding.FragmentStartBinding
 import com.example.luftkvalitet.databinding.FragmentStatistikBinding
 import com.example.luftkvalitet.overview.OverViewModel
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
-
-
-
-// TODO: Rename parameter arguments, choose names that match
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
 
 class statistikFragment : Fragment() {
@@ -48,17 +52,23 @@ class statistikFragment : Fragment() {
 
 
         val entries: ArrayList<BarEntry> = ArrayList()
+        entries.add(BarEntry(0f, 12f))
         entries.add(BarEntry(1f, 4f))
         entries.add(BarEntry(2f, 10f))
         entries.add(BarEntry(3f, 2f))
         entries.add(BarEntry(4f, 15f))
         entries.add(BarEntry(5f, 13f))
         entries.add(BarEntry(6f, 2f))
+        entries.add(BarEntry(7f, 12f))
 
-        val barDataSet = BarDataSet(entries, "")
+
+        val barDataSet = BarDataSet(entries, "test")
         barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+        barDataSet.valueTextSize = 10f
 
         val data = BarData(barDataSet)
+
+
 
         val chart = binding.barChart
 
@@ -83,19 +93,106 @@ class statistikFragment : Fragment() {
 
         //add animation
         chart.animateY(1000)
+        chart.setScaleEnabled(false)
+        chart.setFitBars(true);
 
 
-        //draw chart
+        val labels: ArrayList<String> = ArrayList()
+        labels.add("Jan")
+        labels.add("Feb")
+        labels.add("March")
+        labels.add("April")
+        labels.add("May")
+        labels.add("June")
+        labels.add("July")
+        labels.add("Aug")
+
+
+        chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+
+        chart.setAutoScaleMinMaxEnabled(true);
+
         chart.invalidate()
 
         val xAxis: XAxis = chart.getXAxis()
+
         xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+
+
+        binding.button.setOnClickListener{
+
+            barDataSet.clear()
+
+            barDataSet.addEntry(BarEntry(0f, 12f))
+            barDataSet.addEntry(BarEntry(1f, 13f))
+            barDataSet.addEntry(BarEntry(2f, 14f))
+            barDataSet.addEntry(BarEntry(3f, 15f))
+            barDataSet.addEntry(BarEntry(4f, 12f))
+            barDataSet.addEntry(BarEntry(5f, 13f))
+            barDataSet.addEntry(BarEntry(6f, 14f))
+
+            labels.clear()
+            labels.add("Mon")
+            labels.add("Tue")
+            labels.add("Wed")
+            labels.add("Thur")
+            labels.add("Fri")
+            labels.add("Sat")
+            labels.add("Sun")
+
+
+            chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+
+
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+            chart.invalidate()
+        }
+
+        binding.button2.setOnClickListener{
+
+            barDataSet.clear()
+
+            entries.add(BarEntry(0f, 12f))
+            entries.add(BarEntry(1f, 4f))
+            entries.add(BarEntry(2f, 10f))
+            entries.add(BarEntry(3f, 2f))
+            entries.add(BarEntry(4f, 15f))
+            entries.add(BarEntry(5f, 49f))
+            entries.add(BarEntry(6f, 2f))
+            entries.add(BarEntry(7f, 12f))
+
+            labels.clear()
+            labels.add("Jan")
+            labels.add("Feb")
+            labels.add("March")
+            labels.add("April")
+            labels.add("May")
+            labels.add("June")
+            labels.add("July")
+            labels.add("Aug")
+
+
+            chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+
+            chart.data.notifyDataChanged()
+            chart.notifyDataSetChanged()
+            chart.invalidate()
+        }
+
+
+
+
 
 
 
 
         return view
     }
+
+
+
 
 
 
