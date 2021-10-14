9aaeb346e853e610c8b1fb248cf9a70c4d30a283
kossamu <larssonjohnny@outlook.com>
2021-10-14 09:19:12 +0200

    commit för att uppdatera allt

--- app/src/main/java/com/example/luftkvalitet/ui/main/MyBarDataSet.kt
+++ app/src/main/java/com/example/luftkvalitet/ui/main/MyBarDataSet.kt
@@ -1,33 +1,25 @@
 package com.example.luftkvalitet.ui.main
 
-import android.content.Context
-import android.graphics.Color
-import android.os.Build
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
-import com.example.luftkvalitet.R
-import com.example.luftkvalitet.databinding.FragmentStartBinding
-import com.example.luftkvalitet.databinding.FragmentStatistikBinding
 import com.example.luftkvalitet.network.API
-import com.example.luftkvalitet.overview.OverViewModel
+import com.example.luftkvalitet.ui.main.statistikFragment.Companion.sensor_input
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 
 class MyBarDataSet(yVals: MutableList<BarEntry>?, label: String?) : BarDataSet(yVals, label) {
 
+
     private var lowValue: Float = 10f
     private var highValue: Float = 20f
-    var sensor: String = "NO2"
-
-    @RequiresApi(Build.VERSION_CODES.O)
+    var sensor: String = ""
 
     override fun getEntryIndex(e: BarEntry?): Int {
         TODO("Not yet implemented")
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun getColor(index: Int): Int {
+
+        sensor = sensor_input
+
         if(sensor == "PM2.5"){
 
             lowValue = 9f
@@ -62,7 +54,15 @@ class MyBarDataSet(yVals: MutableList<BarEntry>?, label: String?) : BarDataSet(y
 
 
 
-
-
-
 }
+/*
+        if (getEntryForIndex(index).y < 10f) {
+            return mColors.get(0)
+        } else if (getEntryForIndex(index).y > 20f && getEntryForIndex(index).y ){
+            return mColors.get(1)
+        } else {
+            return mColors.get(2)
+        }
+
+    }
+*/
